36c46c782c9eba0f31fe36c7406f223385b0b95a
KishiEdward <dzidanrh@gmail.com>
2026-05-09 22:24:28 +0700

    feat: tambah custom card widget

--- lib/my_lib_new.dart
+++ lib/my_lib_new.dart
@@ -1,3 +1,4 @@
 library mybuttonlib;
 
 export 'src/my_button_lib.dart';
+export 'src/custom_card_lib.dart';
---
+++ lib/src/custom_card_lib.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatelessWidget {
+  final String title;
+  final String content;
+
+  const CustomCard({super.key, required this.title, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      margin: const EdgeInsets.all(8),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            Text(content),
+          ],
+        ),
+      ),
+    );
+  }
+}
